2b150c74fa33aadbaa75defbe7f9b4b90f4d3e1b
epinet <epinet@excilys.com>
2020-02-11 10:34:00 +0100

    class Connexction SQL

---
+++ src/src/java/main/dao/DAO.java
@@ -0,0 +1,12 @@
+package src.java.main.dao;
+
+import java.sql.Connection;
+
+
+public abstract class DAO<T> {
+	
+	protected Connection connect = null;
+	
+	public DAO()
+
+}
---
+++ src/src/java/main/dao/connexionSQL.java
@@ -0,0 +1,33 @@
+package src.java.main.dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class connexionSQL {
+	
+	private static String url = "jdbc:mysql://localhost:3306";
+
+	private static String user = "nom_utilisateur_choisi";
+
+	private static String password = "mot_de_passe_solide";
+
+	private static Connection connect;
+	
+	
+	
+	public static Connection getInstance(){
+		if(connect == null){
+			try {
+				connect = DriverManager.getConnection(url, user, password);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+		return connect;	
+	}	
+
+
+}
